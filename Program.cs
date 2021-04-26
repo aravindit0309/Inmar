052ce11b80893e3f72a6a7a4b1b1a318b1c65ed5
Aravind <aravindisnice@yahoo.co.in>
2021-04-26 19:24:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = string.Empty;
+            StringBuilder userInputReverse = new StringBuilder();
+            Console.WriteLine("Enter Input: ");
+            userInput = Console.ReadLine();           
+           for(int i = 0; i <= userInput.Length -1; i++)
+            {
+                userInputReverse.Append(userInput[(userInput.Length-1)-i]);
+            }
+            Console.WriteLine("Reversed Input: " + userInputReverse.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
